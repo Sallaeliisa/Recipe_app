881f40c28de9fff8ec7e8fcf79f18ad91edc4f24
Vuorikko Salla <salla.vuorikko@gmail.com>
2021-05-20 17:30:37 +0300

    recipes loading removed

--- src/Components/SingleRecipe.js
+++ src/Components/SingleRecipe.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
+import Spinner from "react-bootstrap/Spinner";
 
 const SingleRecipe = () => {
   const [recipe, setRecipe] = useState();
@@ -18,45 +19,53 @@ const SingleRecipe = () => {
 
   let recipeData = undefined;
 
-  if (!recipe) {
-    recipeData = <div className="loading"><h1>Loading...</h1></div>;
-  }
+  // if (!recipe) {
+  //   recipeData = (
+  //     <div className="loading">
+  //       <Spinner animation="border" role="status">
+  //         <span className="sr-only">Loading...</span>
+  //       </Spinner>
+  //     </div>
+  //   );
+  // }
 
   if (recipe) {
     recipeData = (
       <div className="singleContainer">
-      <div className="singleRecipe">
-        <h2>{recipe.name}</h2>
-        <img src={recipe.image} alt="meal prepared" />
-        <h3>{recipe.description}</h3>
-        <div className="cardText">
-          <p>Category: {recipe.recipeCategory}</p>
-          <p>Servings: {recipe.recipeYield}</p>
-          <p>Cooking time: {recipe.totalTime}</p>
-          <h4>Ingredients:</h4>
-          <ul>
-            {recipe.recipeIngredients.map((item) => {
-              return <p key={recipe.recipeIngredients.indexOf(item)}>{item}</p>;
-            })}
-          </ul>
-          <h4>Instructions:</h4>
-        <ul>
-          {recipe.recipeInstructions.map((item) => {
-            return (
-              <h5 key={recipe.recipeInstructions.indexOf(item)}>
-                Step {recipe.recipeInstructions.indexOf(item) + 1}
-                <div className="item">
-                  <p>{item}</p>
-                </div>
-              </h5>
-            );
-          })}
-        </ul>
+        <div className="singleRecipe">
+          <h2>{recipe.name}</h2>
+          <img src={recipe.image} alt="meal prepared" />
+          <h3>{recipe.description}</h3>
+          <div className="cardText">
+            <p>Category: {recipe.recipeCategory}</p>
+            <p>Servings: {recipe.recipeYield}</p>
+            <p>Cooking time: {recipe.totalTime}</p>
+            <h4>Ingredients:</h4>
+            <ul>
+              {recipe.recipeIngredients.map((item) => {
+                return (
+                  <p key={recipe.recipeIngredients.indexOf(item)}>{item}</p>
+                );
+              })}
+            </ul>
+            <h4>Instructions:</h4>
+            <ul>
+              {recipe.recipeInstructions.map((item) => {
+                return (
+                  <h5 key={recipe.recipeInstructions.indexOf(item)}>
+                    Step {recipe.recipeInstructions.indexOf(item) + 1}
+                    <div className="item">
+                      <p>{item}</p>
+                    </div>
+                  </h5>
+                );
+              })}
+            </ul>
+          </div>
+          <Link to={`/recipes`}>
+            <Button variant="outline-primary">Back to recipes</Button>
+          </Link>
         </div>
-        <Link to={`/recipes`}>
-        <Button variant="outline-primary">Back to recipes</Button>
-        </Link>
-      </div>
       </div>
     );
   }
