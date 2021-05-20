3ca9fbce7881a27ba10fd21294b75b3038eaf1ec
Vuorikko Salla <salla.vuorikko@gmail.com>
2021-05-20 21:27:39 +0300

    recipeapp modified styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+// import { HashRouter, Route } from "react-router-dom";
 import Header from "./Components/Header";
 import Main from "./Components/Main";
 import Footer from "./Components/Footer";
 import "./index.css";
-
 
 function App() {
   return (
